92a5d935e62c63069fe1560f1dff77e7b96bfb8e
Alexander Gunkel <alexandergunkel@gmail.com>
2018-03-25 23:33:39 +0200

    javascript merge-sort

--- sort/javascript/InsertionSort.js
+++ sort/javascript/InsertionSort.js
@@ -1,10 +1,10 @@
 
-this.insertion_sort = function (list_of_things) {
+this.sort = function (list_of_things) {
 	var result = [];
 	var current_length = 0;
 	for (var k = list_of_things.length - 1; k >= 0; k--) {
 		result[current_length] = list_of_things[k];
-		current_length++
+		current_length++;
 		for (var i = current_length - 1; i >= 1 && result[i] < result[i-1]; i--) {
 			var tmp = result[i];
 			result[i] = result[i-1];
