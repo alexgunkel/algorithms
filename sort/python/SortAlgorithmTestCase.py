750347f97395cb4f79ce75be023dd83110cae1cc
Alexander Gunkel <alexandergunkel@gmail.com>
2018-03-25 18:57:50 +0200

    Add python merge sort and performance test

--- sort/python/SortAlgorithmTestCase.py
+++ sort/python/SortAlgorithmTestCase.py
@@ -1,4 +1,5 @@
 import unittest
+import time
 
 
 class SortAlgorithmTestCase(unittest.TestCase):
@@ -13,5 +14,23 @@ class SortAlgorithmTestCase(unittest.TestCase):
         result = []
         self.assertEqual(self.apply_sort(example), result)
 
+    def test_sort_data(self):
+        for name in ['random10', 'random100', 'random1000']:
+            example = self.read_test_data(name)
+            result = self.apply_sort(example)
+            index = 1
+            length = len(result)
+            self.assertEqual(length, len(example))
+            while index < length:
+                self.assertGreaterEqual(result[index], result[index - 1])
+                index = index + 1
+
     def apply_sort(self, list_of_things):
         return list_of_things
+
+    def read_test_data(self, file_name):
+        result = []
+        with open("./data/" + file_name) as file:
+            for line in file:
+                result.append(line.strip())
+        return result
